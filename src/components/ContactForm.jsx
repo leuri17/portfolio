d3d95285dc851be1e18388e658a9da55d508705d
leuri17 <leuri77@gmail.com>
2022-12-23 19:14:34 +0100

    implemented google api for contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,21 +1,43 @@
+/* eslint-disable no-undef */
 import '../styles/ContactForm.css'
 import FloatingLabelInput from './FloatingLabelInput'
+import { useNavigate } from 'react-router-dom'
+import Modal from './Modal'
+import { useField } from '../hooks/useField'
+import { useGapi } from '../hooks/useGapi'
 
 const ContactForm = () => {
-  console.log()
+  const navigate = useNavigate()
+
+  const name = useField({ type: 'text' })
+  const subject = useField({ type: 'text' })
+  const email = useField({ type: 'email' })
+  const message = useField({ type: 'area' })
+
+  const { sendMail, modalValues, closeModal } = useGapi()
+
+  const onModalClose = () => {
+    closeModal()
+    if (modalValues.type === 'success') {
+      navigate('/')
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    sendMail(name.value, email.value, subject.value, message.value)
+  }
+
   return (
-    <form
-      id="contact-form"
-      action={import.meta.env.VITE_CONTACT_FORM_ACTION}
-      method="post"
-      target="_blank">
+    <form id="contact-form" onSubmit={handleSubmit}>
       <h1>Contact</h1>
-      <FloatingLabelInput id="nameInput" name="name" label="Name" />
-      <FloatingLabelInput id="emailInput" name="email" type="email" label="Email" />
-      <FloatingLabelInput id="subjectInput" name="subject" type="text" label="Subject" />
-      <FloatingLabelInput id="messageInput" name="message" type="area" label="Message" />
+      <FloatingLabelInput id="nameInput" name="name" label="Name" {...name} />
+      <FloatingLabelInput id="emailInput" name="email" label="Email" {...email} />
+      <FloatingLabelInput id="subjectInput" name="subject" label="Subject" {...subject} />
+      <FloatingLabelInput id="messageInput" name="message" label="Message" {...message} />
+      {modalValues && <Modal {...modalValues} onClose={onModalClose} />}
       <div className="form-group buttons">
-        <button type="reset">Clear</button>
         <button type="submit">Send</button>
       </div>
     </form>
