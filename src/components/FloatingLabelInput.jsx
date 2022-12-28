17278055077ed4037ebabcbd3a8a584ac0895c7f
leuri17 <leuri77@gmail.com>
2022-12-28 17:30:34 +0100

    Changes in google api code and contact form

--- src/components/FloatingLabelInput.jsx
+++ src/components/FloatingLabelInput.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import '../styles/FloatingLabelInput.css'
 
-const FloatingLabelInput = ({ type = 'text', label, id, name, value, onChange, ...props }) => {
+const FloatingLabelInput = ({ type = 'text', label, id, name, value, onChange, required = false, ...props }) => {
   return (
     <div className="form-group">
       {type === 'area'
@@ -11,12 +11,12 @@ const FloatingLabelInput = ({ type = 'text', label, id, name, value, onChange, .
           id={id}
           value={value}
           onChange={onChange}
-          required
+          required={required}
           rows={6}
           cols={64}></textarea>
           )
         : (
-        <input name={name} type={type} value={value} onChange={onChange} id={id} required />
+        <input name={name} type={type} value={value} onChange={onChange} id={id} required={required} />
           )}
       <label className={value && 'filled'} htmlFor={id}>
         {label}
@@ -31,7 +31,8 @@ FloatingLabelInput.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string,
   value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  required: PropTypes.bool
 }
 
 export default FloatingLabelInput
