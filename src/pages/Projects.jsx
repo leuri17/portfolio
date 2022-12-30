984a326d1c0d2848626f8dc40f36563c08d3e6bf
leuri17 <leuri77@gmail.com>
2022-12-30 11:41:30 +0100

    Created AnimatedBorderContainer component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,11 @@
+import AnimatedBorderContainer from '../components/AnimatedBorderContainer'
 import '../styles/Projects.css'
 
 const Projects = () => {
   return (
-      <div className="box">
-        <h2 style={{ margin: '0' }}>Coming soon...</h2>
-      </div>
+    <AnimatedBorderContainer>
+      <h2 style={{ margin: '0' }}>Coming soon...</h2>
+    </AnimatedBorderContainer>
   )
 }
 
