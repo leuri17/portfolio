7db4e4b389a0a3c5a09817323224be0a733385b9
leuri17 <leuri77@gmail.com>
2023-01-08 01:50:07 +0100

    Slider function on carousel done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,21 +7,21 @@ const Carousel = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [pagesNumber, setPagesNumber] = useState()
 
-  const carouselRef = useRef()
-  const prevBtn = useRef()
-  const nextBtn = useRef()
+  const carouselItemsRef = useRef()
+  const prevBtnRef = useRef()
+  const nextBtnRef = useRef()
 
   useEffect(() => {
     setPagesNumber(parseInt((SKILL_LIST.length / ELEMENTS_PER_PAGE).toFixed()))
   }, [])
 
   useEffect(() => {
-    if (carouselRef.current.classList.contains('next')) {
-      carouselRef.current.classList.toggle('next')
+    if (carouselItemsRef.current.classList.contains('next')) {
+      carouselItemsRef.current.classList.toggle('next')
     }
 
-    if (carouselRef.current.classList.contains('prev')) {
-      carouselRef.current.classList.toggle('prev')
+    if (carouselItemsRef.current.classList.contains('prev')) {
+      carouselItemsRef.current.classList.toggle('prev')
     }
     enableButtons()
   }, [currentPage])
@@ -29,10 +29,10 @@ const Carousel = () => {
   const prevPage = (e) => {
     e.preventDefault()
     disableButtons()
-    carouselRef.current.style.animation = ''
+    carouselItemsRef.current.style.animation = ''
 
     const onAnimationEnd = (ev) => {
-      carouselRef.current.style.animation = 'enter-prev .35s linear'
+      carouselItemsRef.current.style.animation = 'enter-prev .35s linear'
 
       if (currentPage === 1) {
         setCurrentPage(pagesNumber)
@@ -40,20 +40,20 @@ const Carousel = () => {
         setCurrentPage(currentPage - 1)
       }
 
-      carouselRef.current.removeEventListener('animationend', onAnimationEnd)
+      carouselItemsRef.current.removeEventListener('animationend', onAnimationEnd)
     }
 
-    carouselRef.current.addEventListener('animationend', onAnimationEnd)
-    carouselRef.current.classList.toggle('prev')
+    carouselItemsRef.current.addEventListener('animationend', onAnimationEnd)
+    carouselItemsRef.current.classList.toggle('prev')
   }
 
   const nextPage = (e) => {
     e.preventDefault()
     disableButtons()
-    carouselRef.current.style.animation = ''
+    carouselItemsRef.current.style.animation = ''
 
     const onAnimationEnd = (ev) => {
-      carouselRef.current.style.animation = 'enter-next .35s linear'
+      carouselItemsRef.current.style.animation = 'enter-next .35s linear'
 
       if (currentPage === pagesNumber) {
         setCurrentPage(1)
@@ -61,26 +61,46 @@ const Carousel = () => {
         setCurrentPage(currentPage + 1)
       }
 
-      carouselRef.current.removeEventListener('animationend', onAnimationEnd)
+      carouselItemsRef.current.removeEventListener('animationend', onAnimationEnd)
     }
 
-    carouselRef.current.addEventListener('animationend', onAnimationEnd)
-    carouselRef.current.classList.toggle('next')
+    carouselItemsRef.current.addEventListener('animationend', onAnimationEnd)
+    carouselItemsRef.current.classList.toggle('next')
   }
 
   const disableButtons = () => {
-    prevBtn.current.disabled = true
-    nextBtn.current.disabled = true
+    prevBtnRef.current.disabled = true
+    nextBtnRef.current.disabled = true
   }
 
   const enableButtons = () => {
-    prevBtn.current.disabled = false
-    nextBtn.current.disabled = false
+    prevBtnRef.current.disabled = false
+    nextBtnRef.current.disabled = false
+  }
+
+  const handlePointerDown = (downEvent) => {
+    const handlePointerMove = (moveEvent) => {
+      const pointerMovement = moveEvent.clientX - downEvent.clientX
+
+      if (pointerMovement > 6) {
+        prevBtnRef.current.click()
+      } else if (pointerMovement < -6) {
+        nextBtnRef.current.click()
+      }
+
+      carouselItemsRef.current.removeEventListener('pointermove', handlePointerMove)
+    }
+
+    carouselItemsRef.current.addEventListener('pointerup', (ev) => {
+      carouselItemsRef.current.removeEventListener('pointermove', handlePointerMove)
+    })
+
+    carouselItemsRef.current.addEventListener('pointermove', handlePointerMove)
   }
 
   return (
-    <div id="carousel-container">
-      <div id="carousel" ref={carouselRef}>
+    <div id="carousel">
+      <div id="carousel-items" onPointerDown={handlePointerDown} ref={carouselItemsRef}>
         {SKILL_LIST.slice(
           (currentPage - 1) * ELEMENTS_PER_PAGE,
           currentPage * ELEMENTS_PER_PAGE
@@ -94,10 +114,10 @@ const Carousel = () => {
         })}
       </div>
       <div id="carousel-buttons">
-        <button onClick={prevPage} ref={prevBtn}>
+        <button onClick={prevPage} ref={prevBtnRef}>
           &lt;
         </button>
-        <button onClick={nextPage} ref={nextBtn}>
+        <button onClick={nextPage} ref={nextBtnRef}>
           &gt;
         </button>
       </div>
