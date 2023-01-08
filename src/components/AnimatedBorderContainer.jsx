e9589aef8299af1e14d351e630dcf70a22bb2a93
leuri17 <leuri77@gmail.com>
2023-01-08 23:01:41 +0100

    Transitions changed and some more changes

--- src/components/AnimatedBorderContainer.jsx
+++ src/components/AnimatedBorderContainer.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types'
 const AnimatedBorderContainer = ({ firstBorder = '#3282b8', secondBorder = '#50d890', children, ...props }) => {
   return (
     <div className="container" style={{ '--first-border': firstBorder, '--second-border': secondBorder }}>
-      <span></span>
       {children}
+      <span></span>
     </div>
   )
 }
