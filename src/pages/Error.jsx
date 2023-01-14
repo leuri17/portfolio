e1c779312b8e08d25e5d5a99adc2631059f2fb8b
leuri17 <leuri77@gmail.com>
2023-01-14 16:26:22 +0100

    Not found page done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,19 @@
+import '../styles/ErrorPage.css'
+import { useNavigate } from 'react-router-dom'
+
 const Error = () => {
+  const nav = useNavigate()
+
   return (
-    <div>Error</div>
+    <div id="error-page-container">
+      <div id="error-page-card" data-theme="light">
+        <h1>Oops, something is missing</h1>
+        <p>Sorry, the page you are looking for cannot be found.</p>
+        <div>
+          <button onClick={() => { nav(-1) }}>Go back</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
