a34df24e8fda733e154600e4063615e58d6b8f1c
leuri17 <leuri77@gmail.com>
2023-01-14 16:00:34 +0100

    Changes in heading styles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import AnimatedBorderContainer from '../components/AnimatedBorderContainer'
 const Projects = () => {
   return (
     <AnimatedBorderContainer>
-      <h2 style={{ margin: '0' }}>Coming soon...</h2>
+      <h2 style={{ margin: '0', fontWeight: '200' }}>Coming soon...</h2>
     </AnimatedBorderContainer>
   )
 }
