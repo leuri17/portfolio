591c34063dbe048040f5b22de3457943eb034824
leuri17 <leuri77@gmail.com>
2023-02-22 14:51:21 +0100

    i18n configured

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from './context/ThemeContext'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './config/Router'
+import './config/i18n'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
