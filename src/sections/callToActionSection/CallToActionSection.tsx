ebc56ca1be5c0a6373313f33c5267164791f9b9f
leuri17 <leuri77@gmail.com>
2026-04-02 03:47:17 +0200

    feat: cta responsive

--- src/sections/callToActionSection/CallToActionSection.tsx
+++ src/sections/callToActionSection/CallToActionSection.tsx
@@ -9,21 +9,23 @@ import { copyEmail } from '@/lib/utils';
 
 const CallToActionSection = () => {
   return (
-    <Section className="flex flex-col items-center gap-10">
+    <Section className="items-center gap-10">
       <Badge
         variant={'outline'}
         className="border-destructive/50 text-xs text-destructive font-semibold tracking-widest px-3 py-4.5 uppercase gap-1.5"
       >
         <span>Let&apos;s collaborate</span>
       </Badge>
+
       <div className="text-center flex flex-col items-center">
-        <h1 className="text-8xl font-extrabold tracking-tight leading-none mb-8 uppercase">
+        <h1 className="text-5xl font-extrabold tracking-tight leading-none mb-4 uppercase sm:text-6xl md:text-7xl lg:text-8xl">
           Interested in{' '}
           <span className="text-transparent bg-clip-text bg-linear-to-r from-primary to-destructive">
             working together?
           </span>
         </h1>
-        <p className="text-2xl text-muted-foreground leading-relaxed max-w-2xl mb-12">
+
+        <p className="text-xl text-muted-foreground leading-relaxed max-w-2xl md:mb-12 md:text-2xl">
           Software Engineer with <span className="text-slate-100 font-medium">3+ years of experience </span>building
           modern web applications with React and Java. Based in Rotterdam and open to new opportunities.
         </p>
